516e531249a1c0f60f23d535be935680911e1b7e
Alexander Pylypenko <root@hw.com>
2024-04-23 11:42:54 +0300

    add phone, iban fields and tickets income board

---
+++ client/custom/res/templates/dashlets/tickets-income.tpl
@@ -0,0 +1,15 @@
+<div class="tickets-income-container">
+    <div></div>
+    <div class="income-result">
+        <div></div>
+        <div class="" style="padding-right: 5px">
+            <span class="text-success" style="font-size: 1.5em">₴</span>
+            <span class="text-3em text-success text-bold">{{ income }}</span>
+        </div>
+        <div class="income-result-details">
+            <div style="color: var(--state-info-text); margin-bottom: -5px;">{{ profit }}</div>
+            <div style="color: var(--state-info-text);">{{ expenses }}</div>
+        </div>
+    </div>
+    <div></div>
+</div>
---
+++ client/custom/src/views/dashlets/tickets-income.js
@@ -0,0 +1,66 @@
+define('custom:views/dashlets/tickets-income', ['views/dashlets/abstract/base'],  function (Dep) {
+    return Dep.extend({
+        name: 'Tickets Income',
+        template: 'custom:dashlets/tickets-income',
+
+        setup: function() {
+            this.income = 0;
+            this.incomeCash = 0;
+            this.incomeIBAN = 0;
+            this.events = { total: 0, list: [] };
+            this.eventId = this.getOption('eventId');
+
+            this.wait(
+                this.fetchTickets(this.eventId)
+            );
+        },
+
+        fetchEvents: async function() {
+            const eventsCollection = await this.getCollectionFactory().create('Event');
+            this.events = await eventsCollection.fetch();
+
+            console.log(this.events);
+        },
+
+        fetchTickets: async function(eventId) {
+            try {
+                const ticketsCollection = await this.getCollectionFactory().create('Ticket');
+                if (eventId) {
+                    ticketsCollection.where = [{
+                        "type": "equals",
+                        "attribute": "eventId",
+                        "value": eventId,
+                    }];
+                }
+                
+                const tickets = await ticketsCollection.fetch();
+                this.income = this.sumTickets(tickets.list);
+                this.incomeCash = this.sumTickets(
+                    tickets.list.filter(ticket => ticket.paymentMethod === 'cash')
+                );
+                this.incomeIBAN = this.sumTickets(
+                    tickets.list.filter(ticket => ticket.paymentMethod === 'iban')
+                );
+
+                console.log(tickets);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+
+        sumTickets: function(tickets) {
+            let sum = 0;
+            tickets.forEach(ticket => sum += ticket.price);
+            return sum; 
+        },
+
+        data: function() {
+            return {
+                events: this.events.list,
+                income: this.income.toLocaleString('en'),
+                profit: this.incomeCash.toLocaleString('en'),
+                expenses: this.incomeIBAN.toLocaleString('en')
+            }
+        }
+    })
+});
